986ec85f538a7f83038662d83d66e66d48d9e649
laserarjun876 <laserarjun876@gmail.com>
2020-06-06 07:19:14 +0545

    arrow function

--- JavaScript/11_ES6/06_exercise.js
+++ JavaScript/11_ES6/06_exercise.js
@@ -1,11 +1,17 @@
-//Task 6 :Exercise : Filter out only the even numbers from the array
+// //Task 6 :Exercise : Filter out only the even numbers from the array
 
-// const primeNumbers=[1,2,3,4,5,6].filter(/*add the function to filer out the even numbers  */)
-// console.log(primeNumbers)
+// // const primeNumbers=[1,2,3,4,5,6].filter(/*add the function to filer out the even numbers  */)
+// // console.log(primeNumbers)
 
-const isEven=number=>number%2===0; //Used arrow function here 
-const evenNumbers=[1,2,3,4,5,6].filter(isEven)
+// const isEven=number=>number%2===0; //Used arrow function here 
+// const evenNumbers=[1,2,3,4,5,6].filter(isEven)
 
-console.log(evenNumbers)
+// console.log(evenNumbers)
 
 
+
+
+const spices = ['ginger', 'cardamom', 'cinnamon', 'nutmeg'];
+const [first, second, ...other] = spices;
+
+console.log(first, second, other);
