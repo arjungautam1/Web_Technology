d345f9bb78a0e7f916fd37244e34fee7769256ba
laserarjun876 <laserarjun876@gmail.com>
2020-06-07 22:42:03 +0545

    String_interpolation

--- JavaScript/11_ES6/05_exercise.js
+++ JavaScript/11_ES6/05_exercise.js
@@ -1,23 +1,23 @@
 // Task:Exercise:Refractor the code to use the ES6 String utility methods 
-
-// const country="Bulgaris";
-// const city="Sofia";
-// if(country.indexOf("Bulg") >-1)
-// {
-//     console.log("The country starts with Bulg");
-// }
-// if(city.indexOf("So")===0)
-// {
-//     console.log("The name starts with So");
-// }
-// if(city.lastIndexOf("a")===city.length-1)
-// {
-//     console.log("The name ends with a");
-// }
-// console.log(
-//     "The capital of "+country+
-//     "is the city of "+city
-// );
+// Question
+const country="Bulgaris";
+const city="Sofia";
+if(country.indexOf("Bulg") >-1)
+{
+    console.log("The country starts with Bulg");
+}
+if(city.indexOf("So")===0)
+{
+    console.log("The name starts with So");
+}
+if(city.lastIndexOf("a")===city.length-1)
+{
+    console.log("The name ends with a");
+}
+console.log(
+    "The capital of "+country+
+    "is the city of "+city
+);
 
 //This complex code can be done with the very easy String utility methods 
 
